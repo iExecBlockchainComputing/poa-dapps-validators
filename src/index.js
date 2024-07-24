a134698f7caf15ca4c803d080800e198b64e3a66
Gabriel Fournier <gabriel.fournier@iex.ec>
2024-07-24 16:15:50 +0200

    add comment

--- src/index.js
+++ src/index.js
@@ -269,7 +269,7 @@ class AppMainRouter extends Component {
 
   getValidatorsNetworkBranch() {
     console.log('this.state.netId', this.state.netId)
-    console.log('getNetworkBranch(this.state.netId)', getNetworkBranch(this.state.netId))
+    // console.log('getNetworkBranch(this.state.netId)', getNetworkBranch(this.state.netId))
     return this.state.netId ? getNetworkBranch(this.state.netId) : null
   }
 
