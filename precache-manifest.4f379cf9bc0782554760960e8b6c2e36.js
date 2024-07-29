95ebbd5a5569315edee2439d284ef6bc726d85a4
Gabriel Fournier <gabriel.fournier@iex.ec>
2024-07-29 10:08:44 +0200

    Updates

---
+++ precache-manifest.4f379cf9bc0782554760960e8b6c2e36.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "e229c003dae271248ab6",
+    "url": "/static/css/main.bec68933.chunk.css"
+  },
+  {
+    "revision": "e229c003dae271248ab6",
+    "url": "/static/js/main.e229c003.chunk.js"
+  },
+  {
+    "revision": "f758f56136ae5b706d63",
+    "url": "/static/js/1.f758f561.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0b5dc31afd63462b5b2b3b69ddeaa899",
+    "url": "/static/media/core.0b5dc31a.svg"
+  },
+  {
+    "revision": "3ab0ed5ce90939d706cd797113a75220",
+    "url": "/static/media/xdai.3ab0ed5c.svg"
+  },
+  {
+    "revision": "46f8323de801ba6b5bb8a3fb84f1094a",
+    "url": "/static/media/sokol.46f8323d.svg"
+  },
+  {
+    "revision": "d0632f1b30f266f6ddb279fc4eef7750",
+    "url": "/static/media/kovan.d0632f1b.svg"
+  },
+  {
+    "revision": "dba5743239423ef9ecd3ee0ebd630a44",
+    "url": "/static/media/logo.dba57432.svg"
+  },
+  {
+    "revision": "99e1a5e4afccf699a2be3c2f9d0942c3",
+    "url": "/static/media/logo.99e1a5e4.svg"
+  },
+  {
+    "revision": "7049932a64cf49dedccbe13692ddef1d",
+    "url": "/static/media/logo.7049932a.svg"
+  },
+  {
+    "revision": "18db08d6d047e0d64dea8ddd229e7852",
+    "url": "/static/media/logo.18db08d6.svg"
+  },
+  {
+    "revision": "7215ff81a6e48c0be2f70076721c2684",
+    "url": "/static/media/core.7215ff81.svg"
+  },
+  {
+    "revision": "ad4f59254dba06bf06f2c79373cc5ae5",
+    "url": "/static/media/sokol.ad4f5925.svg"
+  },
+  {
+    "revision": "ad4f59254dba06bf06f2c79373cc5ae5",
+    "url": "/static/media/kovan.ad4f5925.svg"
+  },
+  {
+    "revision": "14405d8763a5ef8d6a334f89e6fa1c46",
+    "url": "/static/media/sokol.14405d87.svg"
+  },
+  {
+    "revision": "b17847f3f9b256016ff29149cf578ced",
+    "url": "/static/media/xdai.b17847f3.svg"
+  },
+  {
+    "revision": "6f8f06869adccf4618097b7f20d74142",
+    "url": "/static/media/core.6f8f0686.svg"
+  },
+  {
+    "revision": "14405d8763a5ef8d6a334f89e6fa1c46",
+    "url": "/static/media/kovan.14405d87.svg"
+  },
+  {
+    "revision": "83a47ff96d10d0ab30289ddf2793ef01",
+    "url": "/static/media/dai.83a47ff9.svg"
+  }
+];
